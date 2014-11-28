3ffe7eaebb3f52cc4e767143bad1e38b94afa61f
Karan Goel <karan@goel.im>
2014-11-28 13:56:01 -0800

    implement isCreditCard

--- lib/src/validator.dart
+++ lib/src/validator.dart
@@ -15,6 +15,9 @@ RegExp hexcolor = new RegExp(r'^#?([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$');
 
 RegExp base64 = new RegExp(r'^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{4})$');
 
+RegExp creditCard = new RegExp(r'^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$');
+
+
 Map uuid = {
   '3': new RegExp(r'^[0-9A-F]{8}-[0-9A-F]{4}-3[0-9A-F]{3}-[0-9A-F]{4}-[0-9A-F]{12}$'),
   '4': new RegExp(r'^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$'),
@@ -373,4 +376,36 @@ bool isIn(String str, values) {
   }
 
   return values.indexOf(str) >= 0;
+}
+
+
+// check if the string is a credit card
+bool isCreditCard(String str) {
+  var sanitized = str.replaceAll(r'[^0-9]+', '');
+  if (!creditCard.hasMatch(str)) {
+    return false;
+  }
+
+  // Luhn algorithm
+  int sum = 0;
+  String digit;
+  bool shouldDouble;
+
+  for (int i = sanitized.length - 1; i >= 0; i--) {
+    digit = sanitized.substring(i, (i + 1));
+    int tmpNum = int.parse(digit);
+
+    if (shouldDouble == true) {
+        tmpNum *= 2;
+        if (tmpNum >= 10) {
+            sum += ((tmpNum % 10) + 1);
+        } else {
+            sum += tmpNum;
+        }
+    } else {
+        sum += tmpNum;
+    }
+    shouldDouble = !shouldDouble;
+  }
+  return !!((sum % 10) == 0 ? sanitized : false);
 }
