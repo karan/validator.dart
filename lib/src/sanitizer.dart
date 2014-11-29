15e9f9b5de44dab25f1f13914575584fbf0b95e7
Karan Goel <karan@goel.im>
2014-11-29 02:04:13 -0800

    implement toInt, hacky but should work

--- lib/src/sanitizer.dart
+++ lib/src/sanitizer.dart
@@ -33,3 +33,14 @@ double toFloat(String str) {
 double toDouble(String str) {
   return toFloat(str);
 }
+
+
+// convert the input to an integer, or NaN if the input is not an integer
+int toInt(String str, [radix]) {
+  try {
+    radix = radix == null ? 10 : radix;
+    return int.parse(str, radix:radix);
+  } catch (e) {
+    return double.NAN.toInt();
+  }
+}
