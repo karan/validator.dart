8d95c2757dcfe6c639c77d1251a3a7cc12399a21
Karan Goel <karan@goel.im>
2014-12-01 10:28:48 -0800

    add proper dartdocs for sanitizer

--- lib/src/sanitizer.dart
+++ lib/src/sanitizer.dart
@@ -3,7 +3,7 @@ part of validator;
 Map _default_normalize_email_options = { 'lowercase': true };
 
 
-// convert the input to a string
+/// convert the input to a string
 String toString(input) {
   if (input == null || (input is List && input.length == 0)) {
     input = '';
@@ -12,7 +12,7 @@ String toString(input) {
 }
 
 
-// convert the input to a date, or null if the input is not a date
+/// convert the input to a date, or null if the input is not a date
 DateTime toDate(String str) {
   try {
     return DateTime.parse(str);
@@ -22,7 +22,7 @@ DateTime toDate(String str) {
 }
 
 
-// convert the input to a float, or NaN if the input is not a float
+/// convert the input to a float, or NAN if the input is not a float
 double toFloat(String str) {
   try{
     return double.parse(str);
@@ -31,13 +31,13 @@ double toFloat(String str) {
   }
 }
 
-// convert the input to a float, or NaN if the input is not a float
+/// convert the input to a float, or NAN if the input is not a float
 double toDouble(String str) {
   return toFloat(str);
 }
 
 
-// convert the input to an integer, or NaN if the input is not an integer
+/// convert the input to an integer, or NAN if the input is not an integer
 num toInt(String str, {int radix:10}) {
   try {
     return int.parse(str, radix:radix);
@@ -51,7 +51,8 @@ num toInt(String str, {int radix:10}) {
 }
 
 
-// convert the input to a boolean
+/// convert the input to a boolean Everything except for '0', 'false' and ''
+/// returns true. In [strict] mode only '1' and 'true' return true.
 bool toBoolean(String str, [bool strict]) {
   if (strict == true) {
     return str == '1' || str == 'true';
@@ -60,47 +61,50 @@ bool toBoolean(String str, [bool strict]) {
 }
 
 
-// trim characters (whitespace by default) from both sides of the input
+/// trim characters (whitespace by default) from both sides of the input
 String trim(String str, [String chars]) {
   RegExp pattern = (chars != null) ? new RegExp('^[$chars]+|[$chars]+\$') : new RegExp(r'^\s+|\s+$');
   return str.replaceAll(pattern, '');
 }
 
 
-// trim characters from the left-side of the input
+/// trim characters from the left-side of the input
 String ltrim(String str, [String chars]) {
   var pattern = chars != null ? new RegExp('^[$chars]+') : new RegExp(r'^\s+');
   return str.replaceAll(pattern, '');
 }
 
 
-// trim characters from the right-side of the input
+/// trim characters from the right-side of the input
 String rtrim(String str, [String chars]) {
   var pattern = chars != null ? new RegExp('[$chars]+\$') : new RegExp(r'\s+$');
   return str.replaceAll(pattern, '');
 }
 
 
-// remove characters that do not appear in the whitelist
+/// remove characters that do not appear in the whitelist. The characters are
+/// used in a RegExp and so you will need to escape some chars
 String whitelist(String str, String chars) {
   return str.replaceAll(new RegExp('[^' + chars + ']+'), '');
 }
 
 
-// remove characters that appear in the blacklist
+/// remove characters that appear in the blacklist. The characters are
+/// used in a RegExp and so you will need to escape some chars
 String blacklist(String str, String chars) {
   return str.replaceAll(new RegExp('[' + chars + ']+'), '');
 }
 
 
-// remove characters with a numerical value < 32 and 127
+/// remove characters with a numerical value < 32 and 127. If [keep_new_lines]
+/// is true, newline characters are preserved (\n and \r, hex 0xA and 0xD).
 String stripLow(String str, [bool keep_new_lines]) {
   String chars = keep_new_lines == true ? '\x00-\x09\x0B\x0C\x0E-\x1F\x7F' : '\x00-\x1F\x7F';
   return blacklist(str, chars);
 }
 
 
-// replace <, >, &, ' and " with HTML entities
+/// replace <, >, &, ' and " with HTML entities
 String escape(String str) {
   return (str.replaceAll(new RegExp(r'&'), '&amp;')
              .replaceAll(new RegExp(r'"'), '&quot;')
@@ -110,7 +114,15 @@ String escape(String str) {
 }
 
 
-// canonicalize an email address
+/// canonicalize an email address. options is an [options] which defaults to
+/// `{ lowercase: true }`. With lowercase set to true, the local part of the
+/// email address is lowercased for all domains; the hostname is always
+/// lowercased and the local part of the email address is always lowercased
+/// for hosts that are known to be case-insensitive (currently only GMail).
+/// Normalization follows special rules for known providers: currently,
+/// GMail addresses have dots removed in the local part and are stripped of
+/// tags (e.g. some.one+tag@gmail.com becomes someone@gmail.com) and all
+/// @googlemail.com addresses are normalized to @gmail.com.
 String normalizeEmail(String email, [Map options]) {
   options = _merge(options, _default_normalize_email_options);
   if (isEmail(email) == false) {
